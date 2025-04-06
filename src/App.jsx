ae7c8a541df9f7cfc4ce7fd2ba12a219de8a0c4f
Sardar-Taha <dev.sardartaha@gmail.com>
2025-04-06 21:57:58 +0500

    UI fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,45 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/Home'
-import Contact from './components/Contact'
-import About from './components/About'
-import Navbar from './components/Navbar'
-import { useForm } from 'react-hook-form'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import "./App.css";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
-      element: <><Navbar /><Home /></>,
+      path: "/",
+      element: (
+        <>
+          <Navbar />
+          <Home />
+        </>
+      ),
     },
     {
-      path: '/about',
-      element: <><Navbar /><About /></>,
+      path: "/about",
+      element: (
+        <>
+          <Navbar />
+          <About />
+        </>
+      ),
     },
     {
-      path: '/contact',
-      element: <><Navbar /><Contact /></>,
-    }
-  ])
+      path: "/contact",
+      element: (
+        <>
+          <Navbar />
+          <Contact />
+        </>
+      ),
+    },
+  ]);
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
